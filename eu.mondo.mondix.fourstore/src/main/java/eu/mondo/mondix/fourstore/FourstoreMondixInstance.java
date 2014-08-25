fff8a0f63b13b18831cf7fd81d33c5d65d9577ed
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-25 22:05:17 +0200

    Added parent Maven project and added initial 4store Maven project.

---
+++ eu.mondo.mondix.fourstore/src/main/java/eu/mondo/mondix/fourstore/FourstoreMondixInstance.java
@@ -0,0 +1,24 @@
+package eu.mondo.mondix.fourstore;
+
+import eu.mondo.mondix.core.IMondixInstance;
+import eu.mondo.mondix.core.IMondixRelation;
+import eu.mondo.mondix.core.IUnaryQueryInstance;
+
+public class FourstoreMondixInstance implements IMondixInstance {
+
+	public IMondixRelation getBaseRelationByName(final String relationName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public IMondixRelation getCatalogRelation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public IUnaryQueryInstance getPublishedRelationNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
