48ea461b901a3c3dd07fddaadedca753a1c109b3
bergmann <bergmann@mit.bme.hu>
2014-11-11 17:13:49 +0100

    Generalized test, so that it can be run with parsed queries

--- eu.mondo.mondix.incquery.test/src/eu/mondo/mondix/incquery/test/MyMondixIQTest.java
+++ eu.mondo.mondix.incquery.test/src/eu/mondo/mondix/incquery/test/MyMondixIQTest.java
@@ -12,13 +12,15 @@
 package eu.mondo.mondix.incquery.test;
 
 
+import org.eclipse.incquery.runtime.api.IPatternMatch;
+import org.eclipse.incquery.runtime.api.IQuerySpecification;
 import org.eclipse.incquery.runtime.api.IncQueryEngine;
+import org.eclipse.incquery.runtime.api.IncQueryMatcher;
 import org.eclipse.incquery.runtime.exception.IncQueryException;
 import org.junit.Assert;
 import org.junit.Test;
 
 import eu.mondo.mondix.incquery.MondixScope;
-import eu.mondo.mondix.incquery.runtime.MondixMatcher;
 
 /**
  * @author Bergmann Gabor
@@ -28,26 +30,33 @@ public class MyMondixIQTest {
 	
 	@Test
 	public void test() throws IncQueryException {
+		MyMondixQuery query = new MyMondixQuery();
+		testQuery(query);
+	}
+
+	public static <Match extends IPatternMatch> void testQuery(
+			IQuerySpecification<? extends IncQueryMatcher<Match>> query) 
+					throws IncQueryException 
+	{
 		MyMondixModel model = new MyMondixModel();
 		MondixScope scope = new MondixScope(model.getMondixer());
 		
 		IncQueryEngine engine = IncQueryEngine.on(scope);
-		MyMondixQuery query = new MyMondixQuery();
-		MondixMatcher matcher = engine.getMatcher(query);
+		IncQueryMatcher<Match> matcher = engine.getMatcher(query);
 		
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x1", "y1")));
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x1", "y2")));
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x2", "y1")));
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x2", "y3")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x1", "y1")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x1", "y2")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x2", "y1")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x2", "y3")));
 		Assert.assertEquals(4, matcher.countMatches());
 		
 		model.modifyModel();
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x1", "y1")));
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x1", "y2")));
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x2", "y1")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x1", "y1")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x1", "y2")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x2", "y1")));
 		//Assert.assertTrue(matcher.hasMatch(query.newMatch("x2", "y3")));
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x3", "y2")));
-		Assert.assertTrue(matcher.hasMatch(query.newMatch("x3", "y4")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x3", "y2")));
+		Assert.assertTrue(matcher.hasMatch(matcher.newMatch("x3", "y4")));
 		Assert.assertEquals(5, matcher.countMatches());
 	}
 
